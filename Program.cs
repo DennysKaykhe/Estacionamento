4fcd2b54b80fa79a662c27390dfb9b05b3d0c741
ETEC Adolpho Berezin <e107ti@etec.sp.gov.br>
2025-05-14 22:44:40 -0300

    a

--- Program.cs
+++ Program.cs
@@ -1,4 +1,4 @@
-﻿double perma, esta, valet, lavagem, total;
+﻿double perma, total, HorasAdicionais, ValorDasHorasAdcionais, Diária, Lavagem, valet;
 string tamanho, serviçoV, serviçoL;
 
 Console.Clear();
@@ -8,30 +8,99 @@ Console.WriteLine("--- Estacionamento ---\n");
 Console.Write("Tamanho do veículo (P/G).....: ");
 tamanho = Console.ReadLine()!.ToUpper().Trim();
 
-if (tamanho != "p" && tamanho != "G");
+if (tamanho != "P" && tamanho != "G");
 {
-    Console.WriteLine("Tamanho inválido! Por favor escreva qualquer tecla para voltar a questão.");
+    Console.WriteLine("Tamanho inválido! Por favor aperte qualquer tecla para voltar a questão.");
     Console.ReadKey();
     return;
 }
+    if (tamanho == "P")
+    {
+        HorasAdicionais = 0.16666666666;
+        Diária = 50;
+        Lavagem = 50;
+    }
+    if (tamanho == "G")
+    {
+        HorasAdicionais = 0.33333333333;
+        Diária = 80;
+        Lavagem = 100;
+    }
+
 
 Console.Write("Tempo de permanência (min)...: ");
 perma = Convert.ToDouble(Console.ReadLine());
 
-Console.Write("Serviço de valet (S/N).......: ");
-Console.ReadLine();
+if (perma >= 720)
+{
+    Console.WriteLine("Não aceitamos tempo de permanência acima de 12 horas! Por favor aperte qualquer tecla para voltar a questão.");
+    Console.ReadKey();
+    return;
+}
 
-if ();
+Console.Write("Serviço de valet (S/N).......: ");
+serviçoV = Console.ReadLine()!.ToUpper().Trim();
+
+if (serviçoV != "S" && serviçoV != "N");
+    {
+        Console.WriteLine("Resposta inválida! Por favor aperte qualquer tecla para voltar a questão.");
+        Console.ReadKey();
+        return;
+    }
+if (serviçoV == "S")
+    {
+        valet = 1.20;
+    }
+    else if (serviçoV == "N")
+    {
+        valet = 0;
+    }
 
 Console.Write("Serviço de lavagem (S/N).....: ");
-Console.ReadLine();
+serviçoL = Console.ReadLine()!.ToUpper().Trim();
 
-Console.Write($"\nEstacionamento..: {esta}");
+if (serviçoL != "S" && serviçoL != "N")
+{
+    Console.WriteLine("Resposta inválida! Por favor aperte qualquer tecla para voltar a questão.");
+    Console.ReadKey();
+    return;
+}
+    else if (serviçoL == "S")
+    {
+        // Tudo segue normalmente;
+    }
+        else if (serviçoL == "N")
+    {
+        Lavagem = 0;
+    }
 
-Console.Write($"Valet...........: {valet}");
+double EstacionamentoFinal;
 
-Console.Write($"Lavagem.........: {lavagem}");
+if (perma <= 60)
+{
+    EstacionamentoFinal = 20;
+}
+    else if (perma > 60 && perma < 300)
+    {
+    ValorDasHorasAdcionais = perma * HorasAdicionais;
+    }
+    else if (perma >= 300)
+    {
+        EstacionamentoFinal = Diária;
+    }
+
+EstacionamentoFinal = EstacionamentoFinal * valet;
+
+Console.Write($"\nEstacionamento..: R$ {EstacionamentoFinal}");
+
+valet = EstacionamentoFinal * 0.20;
+
+Console.Write($"Valet...........: R$ {valet}");
+
+Console.Write($"Lavagem.........: R$ {Lavagem}");
 
 Console.WriteLine("--------------------------------\n");
 
-Console.WriteLine($"Total...........: {total}");
+total = EstacionamentoFinal + Lavagem;
+
+Console.WriteLine($"Total...........: R$ {total}");
